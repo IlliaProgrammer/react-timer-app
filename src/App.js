e1d8098ed251787ab2071690e09529f48c9cc219
Illia Fialkivskiji <fialkivskiji@gmail.com>
2023-05-04 21:43:00 +0300

    deploy timer app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      time: props.timeInSeconds || 60,
+      isRunning: false,
+    };
+
+    this.intervalId = null;
+    this.startTimer = this.startTimer.bind(this);
+    this.pauseTimer = this.pauseTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+    this.handleTimeChange = this.handleTimeChange.bind(this);
+  }
+  componentDidMount() {
+    if (this.props.autostart === true) {
+      this.startTimer();
+    }
+  }
+
+  startTimer() {
+    const { step } = this.props;
+  
+    if (!this.state.isRunning) {
+      this.intervalId = setInterval(() => {
+        if (this.state.time <= 0) {
+          clearInterval(this.intervalId);
+          this.setState({ isRunning: false });
+        } else {
+          this.setState({ time: this.state.time - 1 });
+        }
+      }, step);
+  
+      this.setState({ isRunning: true });
+    }
+  }
+
+  pauseTimer() {
+    clearInterval(this.intervalId);
+    this.setState({ isRunning: false });
+  }
+
+  resetTimer() {
+    clearInterval(this.intervalId);
+    this.setState({ time: this.props.timeInSeconds || 60, isRunning: false });
+  }
+
+  handleTimeChange(event) {
+    const newTime = parseInt(event.target.value, 10);
+    this.setState({ time: newTime });
+  }
+
+  render() {
+    const { time, isRunning } = this.state;
+
+
+    return (
+      <div>
+        <h2>Timer: {time}</h2>
+        <label htmlFor="time-input">Set time (seconds): </label>
+        <input type="number" id="time-input" value={time} onChange={this.handleTimeChange} />
+        {isRunning ? (
+          <button onClick={this.pauseTimer}>Pause</button>
+        ) : (
+          <button onClick={this.startTimer}>Start</button>
+        )}
+        <button onClick={this.resetTimer}>Reset</button>
+      
+      </div>
+    );
+  }
 }
-
-export default App;
+export default Timer;
